0200f788a94477ced7122d7086662344d6772ad7
neeols <101673369+neeols@users.noreply.github.com>
2026-03-31 13:36:38 -0700

    delete accidental copy

--- gregorius-drugworks-workspace/gtpersistence/src/main/java/com/wurtzitane/gregoriusdrugworkspersistence/command/sub/GdwPayloadSubcommand.java
+++
@@ -1,4 +0,0 @@
-package com.wurtzitane.gregoriusdrugworkspersistence.command.sub;
-
-public class GdwPayloadSubcommand {
-}
--- gregorius-drugworks-workspace/gtpersistence/src/main/java/com/wurtzitane/gregoriusdrugworkspersistence/inhalation/DurabilityLossMode.java
+++
@@ -1,7 +0,0 @@
-package com.wurtzitane.gregoriusdrugworkspersistence.inhalation;
-
-public enum DurabilityLossMode {
-    FIXED,
-    RANDOM_RANGE,
-    REMAINING_CURVE
-}
